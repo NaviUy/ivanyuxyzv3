c91fcd05df379c7f154766cc12c23714b83abde6
Ivan Yu <ivanscreativity@gmail.com>
2020-06-24 18:19:30 -0700

    6-24-2020 6:19PM

--- src/routes/home.js
+++ src/routes/home.js
@@ -5,56 +5,59 @@ import './pages.css'
 import  { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import  { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-import AboutMe from './aboutme'
-
 const home = props => {
 
     return (
-        // <div className="parent">
-        // <div className="home-container">
-        //     <div className="grid">
-        //         <div className="box box1">
-        //             <div className="logo-container">
-        //                 <h1 className="regular-logo-text">I</h1>
-        //                 <h1 className="regular-logo-text">IV</h1>
-        //                 <h1 className="regular-logo-text">IVA</h1>
-        //                 <h1 className="regular-logo-text">IVAN</h1>
-        //                 <h1 className="regular-logo-text">IVAN Y</h1>
-        //                 <h1 className="regular-logo-text full">IVAN YU</h1>
-        //                 <h1 className="regular-logo-text">IVAN Y</h1>
-        //                 <h1 className="regular-logo-text">IVAN</h1>
-        //                 <h1 className="regular-logo-text">IVA</h1>
-        //                 <h1 className="regular-logo-text">IV</h1>
-        //                 <h1 className="regular-logo-text">I</h1>
-        //             </div>
-        //         </div>
-        //         <div className="box box2">
-        //             <div className="rotating-logo-container">
-        //                 <section class="wrapper">
-        //                     <div className="rotating-text">
-        //                         <div>Software Designer.</div>
-        //                         <div>Recent Graduate.</div>
-        //                         <div>Photographer.</div>
-        //                     </div>
-        //                 </section>
-        //             </div>
-        //         </div>
-        //         <div className="box box3">
-        //             <div className="arrow-button-container">
-        //                 <p className="arrow-button-text">about me</p>
-        //                 <div className="arrow">
-        //                     <FontAwesomeIcon icon={faChevronDown} size="3x"></FontAwesomeIcon>
-        //                 </div>
-        //              </div>
-        //         </div>
-        //     </div>
-        // </div>
-        // <AboutMe />
-        // </div>
 
-        <div className="showcase">
-            <p></p>
+    <div className="container">
+        {/* SECTION A */}
+        <div className="section-a grid">
+            <h1>About Me:</h1>
+            <img src={require("../images/PHOTO-27.jpg")} alt=""></img>
+            <p>My name is Ivan Yu. I am an aspiring software designer who recently graduated
+                from the California State University of Los Angeles with a bachelor’s in computer
+                 science. My goal is to become a full stack web developer.</p>
         </div>
+        {/* SECTION B */}
+        <div className="section-b grid">
+            <h1>Favorite Development Tools:</h1>
+            <ul className="fav-tools">
+                <li>Visual Studio Code</li>
+                <li>Android Studios</li>
+                <li>Eclipse</li>
+                <li>Github</li>
+            </ul>
+        </div>
+        {/* SECTION C */}
+        <div className="section-c grid">
+            <h1>Projects:</h1>
+        </div>
+        {/* SECTION D */}
+        <div className="section-d grid">
+            <h1>Resume:</h1>
+        </div>
+        {/* SECTION E */}
+        <div className="section-e grid">
+            <h1>Hobbies:</h1>
+            <ul className="hobbies">
+                <li>Learning new technologies</li>
+                <li>Photography</li>
+                <li>Swimming</li>
+            </ul>
+        </div>
+        {/* SECTION F */}
+        <div className="section-f grid">
+            <h1>Photography:</h1>
+        </div>
+        {/* SECTION G */}
+        <div className="section-g grid">
+            <h1>Contact Me:</h1>
+        </div>
+        {/* FOOTER */}
+        <div className="footer">
+            <p>Designed by: Ivan Yu</p>
+        </div>
+    </div>
 
 
     );
